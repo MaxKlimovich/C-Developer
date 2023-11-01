6172fb152334312c997c436ba54510c622c375f3
Henry Bronx <klimovich.maks@gmail.com>
2023-11-01 16:12:25 +0300

    add Lesson 4

---
+++ Lesson4/IBits.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Lesson4;
+internal interface IBits
+{
+    //Спроектируйте интерфейс для класса способного устанавливать и
+    //получать  значения отдельных бит в  заданном числе. 
+    // до 21:20
+
+    long Value { get; set; }
+
+    public void SetBit(bool value, int index);
+
+    public bool GetBit(int index);
+
+}
