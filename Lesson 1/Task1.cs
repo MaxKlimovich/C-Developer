3d736bad547eb201e8354c50641922fdddeb2d8c
Henry Bronx <klimovich.maks@gmail.com>
2023-11-01 15:44:24 +0300

    add Lesson1 Tasl1

--- Lesson 1/Task1.cs
+++ Lesson 1/Task1.cs
@@ -1,6 +1,28 @@
 namespace DeveloperC;
 
-public class Task1
+internal class Task1
 {
-    
+    public void Task(string[] args)
+    {
+        if (args.Length > 0)
+        {
+            int[] array = Array.ConvertAll(args, int.Parse);
+            int min = int.MaxValue;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                }
+            }
+
+            System.Console.WriteLine($"min = {min}");
+        }
+        else
+        {
+            System.Console.WriteLine("No arguments");
+        }
+
+    }
 }
